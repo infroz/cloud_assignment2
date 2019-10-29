95a9613e92c6c4f4affc6d69940bd1f422af7ee8
Infroz <infroz98@gmail.com>
2019-10-29 13:55:01 +0100

    ErrorHandler func added

--- ErrorHandler.go
+++ ErrorHandler.go
@@ -1,5 +1,13 @@
 package repocheck
 
-func ErrorHandler() {
+import (
+	"log"
+	"net/http"
+)
 
+func ErrorHandler(err error, w http.ResponseWriter) {
+	if err != nil {
+		log.Println(err)
+		http.Error(w, "Error:", http.StatusBadRequest)
+	}
 }
