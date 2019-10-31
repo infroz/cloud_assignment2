a6c5b82d0d990aed65db2268a6fb41ebc2d1ebac
Infroz <infroz98@gmail.com>
2019-10-31 23:50:41 +0100

    config update

--- config.go
+++ config.go
@@ -1,3 +1,7 @@
 package repocheck
 
+// AuthToken Authentication token for Gitlab
 const AuthToken = "zbHjiZ5PR-cwEosyMqW8"
+
+// FirebaseID id for firebase nosql
+const FirebaseID = "repocheck-a9524"
