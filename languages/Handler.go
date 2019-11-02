6bde7620c940b73981045d28dfcbe5d69b905b92
Infroz <infroz98@gmail.com>
2019-11-02 03:25:35 +0100

    changed Method handler

--- languages/Handler.go
+++ languages/Handler.go
@@ -5,16 +5,14 @@ import (
 	"net/http"
 )
 
-// Handler - Accepts requests for /v1/languages/
+// Handler - Comment
 func Handler(w http.ResponseWriter, r *http.Request) {
 	// Accept only GET requests
 	switch r.Method {
-	case http.MethodGet:
-		// Below Code is executed if Method is GET
 
 	default:
 		// Methods not allowed - Returns 405
-		fmt.Println("HandlerCommits.go: Method not Allowed")
-		http.Error(w, "Method not allowed: ", http.StatusMethodNotAllowed /* 405 */)
+		fmt.Println("HandlerCommits.go: Method not Allowed or Implemented" + r.Method)
+		http.Error(w, "Method not allowed: "+r.Method, http.StatusMethodNotAllowed /* 405 */)
 	}
 }
--- status/Handler.go
+++ status/Handler.go
@@ -1,1 +1,18 @@
 package status
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// Handler - Comment
+func Handler(w http.ResponseWriter, r *http.Request) {
+	// Accept only GET requests
+	switch r.Method {
+
+	default:
+		// Methods not allowed - Returns 405
+		fmt.Println("HandlerCommits.go: Method not Allowed or Implemented" + r.Method)
+		http.Error(w, "Method not allowed: "+r.Method, http.StatusMethodNotAllowed /* 405 */)
+	}
+}
